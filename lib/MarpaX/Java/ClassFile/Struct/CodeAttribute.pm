f12402490b2d8b05868b7e217a700e579e6c7cf2
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-03-10 05:41:26 +0100

    Add required

--- lib/MarpaX/Java/ClassFile/Struct/CodeAttribute.pm
+++ lib/MarpaX/Java/ClassFile/Struct/CodeAttribute.pm
@@ -12,16 +12,17 @@ use Moo;
 
 use MarpaX::Java::ClassFile::Struct::_Types -all;
 use Types::Standard -all;
+use Types::Encodings qw/Bytes/;
 
-has attribute_name_index    => ( is => 'ro', isa => U2 );
-has attribute_length        => ( is => 'ro', isa => U4 );
-has max_stack               => ( is => 'ro', isa => U2 );
-has max_locals              => ( is => 'ro', isa => U2 );
-has code_length             => ( is => 'ro', isa => U4 );
-has code                    => ( is => 'ro', isa => ArrayRef[U1] );
-has exception_table_length  => ( is => 'ro', isa => U2 );
-has exception_table         => ( is => 'ro', isa => ArrayRef[ExceptionTable] );
-has attributes_count        => ( is => 'ro', isa => U2 );
-has attributes              => ( is => 'ro', isa => ArrayRef[AttributeInfo] );
+has attribute_name_index    => ( is => 'ro', required => 1, isa => U2 );
+has attribute_length        => ( is => 'ro', required => 1, isa => U4 );
+has max_stack               => ( is => 'ro', required => 1, isa => U2 );
+has max_locals              => ( is => 'ro', required => 1, isa => U2 );
+has code_length             => ( is => 'ro', required => 1, isa => U4 );
+has code                    => ( is => 'ro', required => 1, isa => Bytes );
+has exception_table_length  => ( is => 'ro', required => 1, isa => U2 );
+has exception_table         => ( is => 'ro', required => 1, isa => ArrayRef[ExceptionTable] );
+has attributes_count        => ( is => 'ro', required => 1, isa => U2 );
+has attributes              => ( is => 'ro', required => 1, isa => ArrayRef[AttributeInfo] );
 
 1;
