53e472e7f13e54ff6c1df9f9d03f7f0ec8daf66c
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-03-15 22:05:02 +0100

    Javapp dev+

--- lib/MarpaX/Java/ClassFile/Data/Scan/Javap.pm
+++ lib/MarpaX/Java/ClassFile/Data/Scan/Javap.pm
@@ -3,19 +3,15 @@ use warnings FATAL => 'all';
 
 package MarpaX::Java::ClassFile::Data::Scan::Javap;
 use Moo;
-use MooX::Options protect_argv => 0, flavour => [qw/pass_through/];
 
-use Module::Find;
-use Perl::OSType qw/is_os_type/;
-my $_HAVE_Win32__Console__ANSI;
-BEGIN {
-  #
-  # Will/Should success only on Win32
-  #
-  $_HAVE_Win32__Console__ANSI = eval 'use Win32::Console::ANSI; 1;' ## no critic qw/BuiltinFunctions::ProhibitStringyEval/
-}
+# ABSTRACT: Data::Scan javap implementation
+
+# VERSION
+
+# AUTHORITY
+
+use MooX::Options protect_argv => 0, flavour => [qw/pass_through/];
 use Scalar::Util qw/reftype blessed looks_like_number/;
-use Term::ANSIColor;
 use Types::Standard -all;
 use Types::Common::Numeric -all;
 #
@@ -24,11 +20,16 @@ use Types::Common::Numeric -all;
 my $_ASCII_PRINT = quotemeta(join('', map { chr } (32..126)));
 my $_NON_ASCII_PRINT_RE = qr/[^$_ASCII_PRINT]/;
 
-# ABSTRACT: Data::Scan javap implementation
-
-# VERSION
-
-# AUTHORITY
+our %ACCESS_FLAG = (
+                    ACC_PUBLIC     => 0x0001,
+                    ACC_FINAL      => 0x0010,
+                    ACC_SUPER      => 0x0020,
+                    ACC_INTERFACE  => 0x0200,
+                    ACC_ABSTRACT   => 0x0400,
+                    ACC_SYNTHETIC  => 0x1000,
+                    ACC_ANNOTATION => 0x2000,
+                    ACC_ENUM       => 0x4000
+                   );
 
 =head1 DESCRIPTION
 
@@ -52,9 +53,7 @@ MarpaX::Java::ClassFile::Data::Scan::Javap is an implementation of the L<Data::S
 #
 # Internal variables
 #
-has _currentLevel => (is => 'rwp', isa => PositiveOrZeroInt|Undef);
-has _colors_cache => (is => 'rwp', isa => Undef|HashRef[Str|Undef]);
-
+has _currentLevel => (is => 'rwp', isa => PositiveOrZeroInt, default => sub { 0 });
 
 =head1 CONSTRUCTOR OPTIONS
 
@@ -245,193 +244,74 @@ option withutf8output => (
                      doc => 'Say if your terminal is capable to print UTF-8 characters. When it is not, any character outside of ASCII, or being a control character, will be escaped to "\\x{codepointer}". Default is a false  value',
                      default => sub { 0 });
 
-=head2 Bool withcolor
-
-Use ANSI colors. Default is a false value if $ENV{ANSI_COLORS_DISABLED} exists, else a true value if $ENV{ANSI_COLORS_ENABLED} exists, else a false value if L<Win32::Console::ANSI> cannot be loaded and you are on Windows, else a true value.
-
-=cut
-
-my $_canColor = __PACKAGE__->_canColor;
-
-option withcolor => (
-                     is => 'ro',
-                     isa => Bool,
-                     negativable => 1,
-                     doc => 'ANSI colorized output. Option is negativable with --no-withcolor. Default is a ' . ($_canColor ? 'true' : 'false') . ' value.',
-                     default => sub { return $_canColor });
-
-=head2 Bool withdefcolor
-
-Definition of ANSI colors.
-
-=cut
-
-my @struct =
-  sort
-  map { (split(/::/, $_))[-1] }
-  grep { /::[A-Z][^:]*$/}
-  findallmod('MarpaX::Java::ClassFile::Struct');
-
-my $_DEFCOLOR =
-  '{' .
-  join(',',
-       '"ConstantUtf8Info":green',
-       '"CodeAttribute":yellow'
-      )
-  . '}';
-option withdefcolor => (
-                        is => 'ro',
-                        json => 1,
-                        doc => "ANSI color definition. Form is a JSON string.\n\n    Supported keys are: " . join(', ', @struct) . "\n\n    Default is: $_DEFCOLOR.",
-                        default => sub { return {} });
-
 sub dsstart {
-  print STDERR "DSSTART " . join(' ', map { $_ // '<undef>'} @_[1..$#_]) . "\n";
-  my ($self) = @_;
-
-  $self->_set__currentLevel(0);
-  #
-  # Precompute color attributes
-  #
-  $self->_set__colors_cache({});
-  if ($self->withcolor) {
-    foreach (keys %{$self->withdefcolor}) {
-      my $color = $self->withdefcolor->{$_};
-      if (defined($color)) {
-        my $colored = colored('dummy', $color);
-        #
-        # ANSI color spec is clear: attributes before the string, followed by
-        # the string, followed by "\e[0m". We do not support the eventual
-        # $EACHLINE hack.
-        #
-        if ($colored =~ /(.+)dummy\e\[0m$/) {
-          $self->{_colors_cache}->{$_} = substr($colored, $-[1], $+[1] - $-[1])
-        } else {
-          $self->{_colors_cache}->{$_} = undef
-        }
-      } else {
-        $self->{_colors_cache}->{$_} = undef
-      }
-    }
-  } else {
-    foreach (keys %{$self->colors}) {
-      $self->{_colors_cache}->{$_} = undef
-    }
-  }
 }
 
 sub dsopen {
-  print STDERR "DSOPEN " . join(' ', map { $_ // '<undef>'} @_[1..$#_]) . "\n";
   my ($self, $item) = @_;
 
+  $self->_print(blessed($item));
   $self->_set__currentLevel($self->_currentLevel + 1);
 }
 
 sub dsread {
-  print STDERR "DSREAD " . join(' ', map { $_ // '<undef>'} @_[1..$#_]) . "\n";
   my ($self, $item) = @_;
 
-  my $blessed = blessed($item) // '';
-  my $struct = (split(/::/, $blessed))[-1] // '';
-  my $method = length($struct) ? $self->can("_$struct") : undef;
-
-  if ($method) {
-    return $self->$method($item)
+  my $blessed = blessed($item);
+  if ($blessed && reftype($item) ne 'SCALAR') {
+    my $method = $self->can('_' . (split(/::/, $blessed))[-1]);
+    if ($method) {
+      return $self->$method($item)
+    } else {
+      return \@{$item}
+    }
   } else {
-    return $self->_dsread($item)
+    return $self->_item($item);
   }
 }
 
 sub dsclose {
-  print STDERR "DSCLOSE " . join(' ', map { $_ // '<undef>'} @_[1..$#_]) . "\n";
   my ($self) = @_;
 
   $self->_set__currentLevel($self->_currentLevel - 1);
 }
 
 sub dsend {
-  print STDERR "DSEND " . join(' ', map { $_ // '<undef>'} @_[1..$#_]) . "\n";
-  my ($self) = @_;
-
-  $self->_set__currentLevel(undef);
-  $self->_set__colors_cache(undef);
 }
 
-sub _dsread {
+sub _item {
   my ($self, $item) = @_;
 
-  my $blessed = blessed($item) // '';
-  my $reftype = reftype($item) // '';
-
-  my @rc = ();
-  if ($reftype eq 'SCALAR') {
-    $self->_print([$blessed, "%s %s", $blessed, ${$item}]);
-  } elsif ($reftype eq 'HASH') {
-    $self->_print([$blessed, "%s", $blessed]);
-    @rc = sort keys %{$item};
-  } elsif ($reftype eq 'ARRAY') {
-    $self->_print([$blessed, "%s", $blessed]);
-    @rc = @{$item};
+  my $blessed = blessed($item);
+  if ($blessed) {
+    $self->_print('%s %s', $blessed, ${$item});
+  } else {
+    $self->_print($item // 'undef');
   }
-
-  \@rc
+  return
 }
 
 sub _ClassFile {
   my ($self, $item) = @_;
 
-  $self->_print(['', 'magic               = 0x%x', $item->magic]);
-  $self->_print(['', 'minor version       = %d', $item->minor_version]);
-  $self->_print(['', 'major version       = %d', $item->minor_version]);
-  $self->_print(['', 'access flags        = %d', $item->access_flags]);
+  $self->_print('magic               = 0x%x', $item->magic);
+  $self->_print('minor version       = %d', $item->minor_version);
+  $self->_print('major version       = %d', $item->minor_version);
+  my $access_flags = $item->access_flags;
+  $self->_print('access flags        = %s',
+                 join(', ',
+                      grep { ($ACCESS_FLAG{$_} & $access_flags) == $ACCESS_FLAG{$_} }
+                      sort { $ACCESS_FLAG{$a} <=> $ACCESS_FLAG{$b} }
+                      keys %ACCESS_FLAG));
 
   return [$item->constant_pool, $item->fields, $item->methods, $item->attributes]
 }
 
-sub _canColor {
-  my ($class) = @_;
-  #
-  # Mimic Data::Printer use of $ENV{ANSI_COLORS_DISABLED}
-  #
-  return 0 if exists($ENV{ANSI_COLORS_DISABLED});
-  #
-  # Add the support of ANSI_COLORS_ENABLED
-  #
-  return 1 if exists($ENV{ANSI_COLORS_ENABLED});
-  #
-  # that has precedence on the Windows check, returning 0 if we did not load Win32::Console::ANSI
-  #
-  return 0 if (is_os_type('Windows') && ! $_HAVE_Win32__Console__ANSI);
-  return 1
-}
-
-sub _colored {
-  my ($self, $what, $fmt, @args) = @_;
-
-  my $desc = sprintf($fmt, @args);
-
-  if (! $self->withutf8output) {
-    #
-    # Detect any non ANSI character and enclose result within ""
-    #
-    $desc =~ s/$_NON_ASCII_PRINT_RE/sprintf('\\x{%x}', ord(${^MATCH}))/egpo;
-  }
-  if ($self->withcolor) {
-    #
-    # We know that _colors_cache is a HashRef, and that _lines is an ArrayRef
-    #
-    my $color_cache = $self->{_colors_cache}->{$what};  # Handled below if it does not exist or its value is undef
-    $desc = $color_cache . $desc . "\e[0m" if (defined($color_cache))
-  }
-
-  $desc
-}
-
 sub _print {
-  my ($self, @desc) = @_;
+  my ($self, $format, @args) = @_;
 
   my $indent = $self->withindent x $self->_currentLevel;
-  my $output = $indent . join('', map { $self->_colored(@{$_}) } @desc) . "\n";
+  my $output = $indent . sprintf($format, @args) . "\n";
   my $handle = $self->handle;
   if (blessed($handle) && $handle->can('print')) {
     $handle->print($output)
