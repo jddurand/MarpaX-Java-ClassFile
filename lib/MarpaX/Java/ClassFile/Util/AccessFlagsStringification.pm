384ef559bcd486e8b4c4ddeeaa69f403ad325dcb
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-03-28 18:25:57 +0200

    Util/AccessFlagsStringification.pm: Added "Class"

--- lib/MarpaX/Java/ClassFile/Util/AccessFlagsStringification.pm
+++ lib/MarpaX/Java/ClassFile/Util/AccessFlagsStringification.pm
@@ -56,6 +56,19 @@ my %_ACCESS_FLAG = (
                      [ ACC_FINAL        => 0x0010 ],
                      [ ACC_SYNTHETIC    => 0x1000 ],
                      [ ACC_MANDATED     => 0x8000 ]
+                    ],
+                    'Class' =>
+                    [
+                     [ ACC_PUBLIC       => 0x0001 ],
+                     [ ACC_PRIVATE      => 0x0002 ],
+                     [ ACC_PROTECTED    => 0x0004 ],
+                     [ ACC_STATIC       => 0x0008 ],
+                     [ ACC_FINAL        => 0x0010 ],
+                     [ ACC_INTERFACE    => 0x0200 ],
+                     [ ACC_ABSTRACT     => 0x0400 ],
+                     [ ACC_SYNTHETIC    => 0x1000 ],
+                     [ ACC_ANNOTATION   => 0x2000 ],
+                     [ ACC_ENUM         => 0x4000 ]
                     ]
 );
 
